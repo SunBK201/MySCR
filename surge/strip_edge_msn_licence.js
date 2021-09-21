2dc4dd390811f63342934b7cf34654012e9569e9
SunBK201 <sunbk201gm@gmail.com>
2021-09-21 12:48:23 +0800

    Update strip_edge_msn_licence.js

--- surge/strip_edge_msn_licence.js
+++ surge/strip_edge_msn_licence.js
@@ -1,5 +1,5 @@
 let body = $response.body;
 
-body = body.replace('viewport-fit=cover">', 'viewport-fit=cover"><meta name="apple-mobile-web-app-capable" content="yes" /><meta name="apple-touch-fullscreen" content="yes" /><link href="https://www.sunbk201.site/hackjs/age.jpg" rel="apple-touch-icon-precomposed"><link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="https://www.sunbk201.site/hackjs/apple-launch-828x1792.png"><link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="https://www.sunbk201.site/hackjs/apple-launch-1536x2048.png">')
+body = body.replace('增值电信业务经营许可证: 合字B2-20090007', '')
 
 $done({body})
